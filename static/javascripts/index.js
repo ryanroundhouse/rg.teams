28955f02173923949aa7404158b9176508a1cbad
Ryan Graham <badmrgraham@gmail.com>
2019-08-15 01:07:28 +0000

    added some simple javascript to toggle between 2 divs.  a bit of refactoring too.

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,5 +1,15 @@
 var index = {
     scheduleSelectionOnClicked : function (selectedSchedule){
-        console.log(selectedSchedule);
+        var selectionFormGroupItems = document.getElementsByClassName('selection-form-group');
+        //console.log(selectionFormGroupItems);
+        for (var i = 0; i < selectionFormGroupItems.length; i++){
+            //console.log("comparing " + selectionFormGroupItems[i].className + " to " + selectedSchedule);
+            if (selectionFormGroupItems[i].id === selectedSchedule){
+                selectionFormGroupItems[i].style.display = "block";
+            }
+            else{
+                selectionFormGroupItems[i].style.display = "none";
+            }
+        }
     }
 }
