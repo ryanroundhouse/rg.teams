9d88e139406c8b0c2aee360bdccf568b75823bd1
Ryan Graham <rg@ryangraham.ca>
2020-02-29 22:34:33 -0500

    refactored code to be laid out by component. refactored Route ->API ->DAO.  Included awaitasync so that that would work.

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -1,42 +1,27 @@
 var express = require('express');
-var Game = require('../models/game');
+var Game = require('../components/game/gameAPI');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  Game.find({}, 'date', function(err, games){
-    if (err) 
-    {
-        return handleError(err);
-    }
-    res.render('createSchedule', { title: 'Hey', message: 'Hello there!', games: games })
-  })
+router.get('/', async function(req, res, next) {
+  const games = await Game.getAll();
+  res.render('createSchedule', { title: 'Hey', message: 'Hello there!', games: games })
 });
 
-router.get('/team', function(req, res, next) {
-  Game.find({}, 'date', function(err, games){
-    if (err) 
-    {
-        return handleError(err);
-    }
-    res.render('teamPage', { games: games })
-  })
+router.get('/team', async function(req, res, next) {
+  const games = await Game.getAll();
+  res.render('teamPage', { games: games });
 });
 
-router.get('/team/game/:id', function(req, res, next) {
-  Game.findOne({_id: req.params.id}, function(err, game){
-    if (err) 
-    {
-        return handleError(err);
-    }
-    if (game == null)
-    {
-      res.redirect('/team');
-    }
-    else
-    {
-      res.render('gamePage', { game: game });
-    }
-  })
+router.get('/team/game/:id', async function(req, res, next) {
+  const game = await Game.getById(req.params.id);
+  if (game == null)
+  {
+    res.redirect('/team');
+  }
+  else
+  {
+    res.render('gamePage', { game: game });
+  }
 });
 
 module.exports = router;
