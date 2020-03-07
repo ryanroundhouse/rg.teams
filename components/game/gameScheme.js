91dcb98e9786d26c61eaaabc55d79b864c1bcd77
Ryan Graham <rg@ryangraham.ca>
2020-03-06 21:06:20 -0500

    added attendanceSchema for games

--- components/game/gameScheme.js
+++ components/game/gameScheme.js
@@ -2,13 +2,22 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+var attendanceSchema = new Schema(
+  {
+    team: {type: mongoose.Schema.Types.ObjectId, ref: 'Person'},
+    status: {type: String},
+    comment: {type: Comment},
+  }
+);
+
 var GameSchema = new Schema(
     {
         date: {type: Date},
         team: {type: mongoose.Schema.Types.ObjectId, ref: 'Team'},
         teamMembers: [String],
+        attendance: [attendanceSchema],
     }
-)
+);
 
 // Virtual for game's URL
 GameSchema.virtual('url').get(function () {
